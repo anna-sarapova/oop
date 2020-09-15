5a17f870342c0301cbfb657a7ac8664a5c8ac8c1
anna-sarapova <sarapova.ana@isa.utm.md>
2020-09-15 20:01:57 +0300

    Added Monitor class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import com.company.lab1.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        Monitor mon1 = new Monitor("white", 300, 4000, 2000);
+        Monitor mon2 = new Monitor("Black", 300, 1920, 1080);
 
+        System.out.println("First\tSecond");
+        System.out.println(mon1.getColor() + "\t" + mon2.getColor());
+        if(mon1.getResolution() > mon2.getResolution()){
+            System.out.println(mon1.getResolution() + " > " + mon2.getResolution());
+        } else if (mon1.getResolution() < mon2.getResolution()) {
+            System.out.println(mon1.getResolution() + " < " + mon2.getResolution());
+        } else {
+            System.out.println("  " + mon1.getResolution() + " = " + mon2.getResolution());
+        }
     }
 }
---
+++ src/com/company/lab1/Monitor.java
@@ -0,0 +1,49 @@
+package com.company.lab1;
+
+public class Monitor {
+    private String color = "Grey";
+    private int resolution = 300;
+    private int width = 1920;
+    private int height = 1080;
+
+    public Monitor(String color, int resolution, int width, int height){
+        this.color = color;
+        this.resolution = resolution;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setResolution(int resolution) {
+        this.resolution = resolution;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+}
